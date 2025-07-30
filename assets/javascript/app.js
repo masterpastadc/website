02e9b1116d8d29272c8dfd5717d4264e37e98a91
masterpastadc <lewisvigus2009@gmail.com>
2025-07-30 21:19:34 +0100

    Add files via upload

---
+++ assets/javascript/app.js
@@ -0,0 +1,38 @@
+
+
+class _app {
+  id = 0;
+  videoElement = null;
+  audioElement = null;
+  musicVolume = 0.17;
+  musicFadeIn = 200;
+  skippedIntro = false;
+  backgroundToggler = false;
+  shouldIgnoreVideo = false;
+  effects = ['bounce', 'flash', 'pulse', 'rubberBand', 'shake', 'swing', 'tada', 'wobble', 'jello'];
+  brandDescription = ['c++ dev', 'very halal', 'weed addict', 'the one and only'];
+
+  iconChanger = (urls, delay) => {
+    if (!urls) return;
+
+    delay = delay || 2000;
+
+    let counter = 0;
+
+    setInterval(() => {
+      if (counter < urls.length) {
+        const link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+
+        link.type = 'image/x-icon';
+        link.rel = 'shortcut icon';
+        link.href = urls[counter];
+
+        document.getElementsByTagName('head')[0].appendChild(link);
+      } else counter = 0;
+
+      ++counter;
+    }, delay);
+  };
+}
+
+const app = new _app();
